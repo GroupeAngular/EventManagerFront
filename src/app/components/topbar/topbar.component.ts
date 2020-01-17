3558987ffdc08f91b9aac25877e952f5a0a8a77c
MatthieuBlm <matthieu.bellamy@isabo.ai>
2020-01-17 17:24:46 +0100

    setup search dropdown

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from 'src/app/services/event.service';
+import { Event } from 'src/app/classes/event';
 
 @Component({
   selector: 'app-topbar',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
-  constructor() { }
+  searchResult: Event[];
+
+  constructor(private eventService: EventService) { }
 
   ngOnInit() {
+    this.searchResult = [];
+  }
+
+  onSearchChange(pattern: string): void {
+    this.eventService.findAllLike(pattern)
+      .subscribe(events => this.searchResult = events);
   }
 
 }
